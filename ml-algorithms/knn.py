1cd8dbcad200b342764595ec0589eee69701be62
Tharun007-TK <tharunkumarvmt@gmail.com>
2025-10-09 16:20:31 +0530

    Initial commit of KNN algotirthm With License(MIT)

--- ml-algorithms/knn.py
+++ ml-algorithms/knn.py
@@ -25,3 +25,29 @@ class KNN:
         k_nearest_labels = [self.y_train[i] for i in k_indices]
         #return the most common class label, majority vote
         most_common = Counter(k_nearest_labels).most_common(1)
+        return most_common[0][0]
+    
+    if __name__ == "__main__":
+        # Imports
+        from matplotlib.colors import ListedColormap
+        from sklearn import datasets
+        from sklearn.model_selection import train_test_split
+
+        cmap = ListedColormap(["#FF0000", "#00FF00", "#0000FF"])
+
+        def accuracy(y_true, y_pred):
+            accuracy = np.sum(y_true == y_pred) / len(y_true)
+            return accuracy
+
+        iris = datasets.load_iris()
+        X, y = iris.data, iris.target
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=1234
+        )
+
+        k = 3
+        clf = KNN(k=k)
+        clf.fit(X_train, y_train)
+        predictions = clf.predict(X_test)
+        print("KNN classification accuracy", accuracy(y_test, predictions))
